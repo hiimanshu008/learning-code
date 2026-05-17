3b68071311038aa8eb4f5b7ff6ece50ad95f9cf3
Himanshu Singh <107603157+hiimanshu008@users.noreply.github.com>
2026-05-17 16:39:07 +0530

    Update MaxInShortestPath.java

--- Beginner/Pre-Prequisite/Graph/MaxInShortestPath.java
+++ Beginner/Pre-Prequisite/Graph/MaxInShortestPath.java
@@ -7,16 +7,17 @@ public class MaxInShortestPath {
     public static int[] maxInShortest(List<List<Integer>> list, int[] value, int n){
         int dist[] = new int[n+1];
         int[] fiveCount = new int[n+1];
+        Arrays.fill(dist,-1)
 
         Queue<Integer> q = new LinkedList<>();
         q.offer(1);
-        dist[1] = 1;
+        dist[1] = 0;
         fiveCount[1] = value[1]==5 ? 1 : 0;
 
         while (!q.isEmpty()) {
             int node = q.poll();
             for(int ne : list.get(node)){
-                if(dist[ne]==0){
+                if(dist[ne]==-1){
                     dist[ne] = dist[node]+1;
                     fiveCount[ne] = fiveCount[node] + (value[ne]==5 ? 1 : 0);
                     q.offer(ne);
